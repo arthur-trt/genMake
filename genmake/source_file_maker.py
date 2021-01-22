454d9840540c444249f91dab92580bdd72d19560
arthur-trt <atrouill@student.42.fr>
2021-01-22 20:50:38 +0100

    Left a debug print because i'm a fucking idiot

--- genmake/source_file_maker.py
+++ genmake/source_file_maker.py
@@ -94,7 +94,6 @@ def populate_sources():
 
 	build_source_file(source_file)
 	max_lenght = obtain_max_lenght(source_file)
-	print (max_lenght)
 	beautify_file(sources_file_path, max_lenght + 5)
 	source_file.close()
 	print(Fore.GREEN + "✅ Makefile updated!" + Style.RESET_ALL)
