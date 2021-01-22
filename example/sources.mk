d60cf5fc46aaa4ad2092f1a04c5205882bac23e5
arthur-trt <atrouill@student.42.fr>
2021-01-22 19:50:17 +0100

    Add example

---
+++ example/sources.mk
@@ -0,0 +1,31 @@
+## SOURCES.MK ##
+
+## TEST ##
+
+SOURCES		+=                                  		\
+		test/file.c                                	\
+
+
+## TEST/TEST2 ##
+
+SOURCES		+=                                  		\
+		test/test2/file5.c                         	\
+		test/test2/file4.c                         	\
+
+
+## TEST/TEST2/TEST3 ##
+
+SOURCES		+=                                  		\
+		test/test2/test3/superlongfilefortesting.c 	\
+		test/test2/test3/file6.c                   	\
+		test/test2/test3/file7.c                   	\
+
+
+## TEST/TEST1 ##
+
+SOURCES		+=                                  		\
+		test/test1/file1.c                         	\
+		test/test1/file2.c                         	\
+		test/test1/file3.c                         	\
+
+
