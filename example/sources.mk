1d68adc3792e718fc20b7d645234b2872e485ace
Arthur-TRT <atrouill@student.42.fr>
2021-09-08 11:24:10 +0200

    Update example according to new version

--- example/sources.mk
+++ example/sources.mk
@@ -4,32 +4,29 @@
 
 SOURCES		+=                                       		\
 		test/file.c                                     	\
-		test/file_bonus.c                               	\
 
 
 ## TEST/TEST1 ##
 
 SOURCES		+=                                       		\
-		test/test1/file3.c                              	\
-		test/test1/file2.c                              	\
-		test/test1/file_bonus.c                         	\
 		test/test1/file1.c                              	\
+		test/test1/file2.c                              	\
+		test/test1/file3.c                              	\
 
 
 ## TEST/TEST2 ##
 
 SOURCES		+=                                       		\
-		test/test2/file5.c                              	\
 		test/test2/file4.c                              	\
-		test/test2/file2_bonus.c                        	\
+		test/test2/file5.c                              	\
 
 
 ## TEST/TEST2/TEST3 ##
 
 SOURCES		+=                                       		\
+		test/test2/test3/file6.c                        	\
 		test/test2/test3/file7.c                        	\
 		test/test2/test3/superlongfilefortesting.c      	\
-		test/test2/test3/file6.c                        	\
 
 
 ## BONUS ##
